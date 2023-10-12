eb89522b2dc274d80f88de9426ec9e41f624659b
Prashant Tiwari <prashant.st8@gmail.com>
2023-10-12 17:42:13 +0530

    feature eng updated for file path

--- Scripts/Text/FeatureEng.py
+++ Scripts/Text/FeatureEng.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
@@ -18,7 +19,27 @@ def EngineerFeatures():
     data['Total Hashtags'] = data['Hashtags'].str.split(' ').apply(lambda x: len(x) if x[0] != '' else 0)
     data['Hashtags'].replace('', np.nan, inplace=True)
 
-    data.to_csv("Data/Text/Engineered.csv", index=False)
+    # data.to_csv("Data/Text/Engineered.csv", index=False)
+
+    negative_images = os.listdir("Data/Images/Negative")
+    neutral_images = os.listdir("Data/Images/Neutral")
+    positive_images = os.listdir("Data/Images/Positive")
+
+    def update_filename(row):
+        filename = row['File Name']
+        if filename in negative_images:
+            return f"Negative/{filename}"
+        elif filename in neutral_images:
+            return f"Neutral/{filename}"
+        elif filename in positive_images:
+            return f"Positive/{filename}"
+        else:
+            return filename
+
+    data['File Name'] = data['File Name'].str.replace('.txt', '.jpg')
+    data['File Name'] = data.apply(update_filename, axis=1)
+
+    data.to_csv("Data/Text/Engineered2.csv", index=False)
 
 if __name__ == '__main__':
     EngineerFeatures()
