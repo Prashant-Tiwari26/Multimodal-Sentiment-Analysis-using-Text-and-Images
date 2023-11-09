beeaeaf0b4492610ab5ddcf2c100e9fce2ddcd47
Prashant Tiwari <prashant.st8@gmail.com>
2023-11-09 16:27:41 +0530

    word indexing for glove

--- Utils/nlp.py
+++ Utils/nlp.py
@@ -10,6 +10,18 @@ from nltk.corpus import wordnet, stopwords
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+def get_word_to_index(glove_path:str):
+    embeddings_index = {}
+    with open(glove_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            values = line.split()
+            word = values[0]
+            coefs = np.asarray(values[1:], dtype='float32')
+            embeddings_index[word] = coefs
+
+    word_to_index = {word: i for i, word in enumerate(embeddings_index.keys())}
+    return word_to_index
+
 def pad_sequence(sequence, max_seq_length):
     if len(sequence) > max_seq_length:
         return sequence[:max_seq_length]
